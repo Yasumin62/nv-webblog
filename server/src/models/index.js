628a78322dc310180515eff092577b74e0858b10
Yasumin62 <yasumins62@nu.ac.th>
2021-08-06 21:15:29 +0700

    add

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,14 +1,14 @@
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const config = require('../config/config');
+const fs = require('fs')//file system
+const path = require('path')
+const Sequelize = require('sequelize')
+const config = require('../config/config')
 const db = {}
 
-const sequelize = new Sequelize(
-    config.db.database,
-    config.db.user,
-    config.db.password,
-    config.db.options
+const sequelize = new Sequelize (
+  config.db.database,
+  config.db.user,
+  config.db.password,
+  config.db.options
 )
 
 fs.readdirSync(__dirname)
@@ -16,11 +16,12 @@ fs.readdirSync(__dirname)
     file !== 'index.js'
   )
   .forEach((file) => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
+    //const model = sequelize.import(path.join(__dirname, file))old version
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)//new version
     db[model.name] = model;
   })
 
-  db.sequelize = sequelize;
-  db.Sequelize = Sequelize;
+  db.sequelize = sequelize
+  db.Sequelize = Sequelize
 
   module.exports = db;
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,10 +1,9 @@
-const UserControllers = require('./controllers/UserControllers')
-const UserController = require('./controllers/UserControllers')
+const UserController = require('./controllers/UserController')
 
-module.exports = (app) =>{
-    app.post('/user',UserControllers.create);
-    app.put('/user/:userId',UserControllers.put);
-    app.delete('./user/:userId',UserControllers.remove);
-    app.get('/user/:userId',UserControllers.show);
-    app.get('/users',UserControllers.index);
+module.exports = (app) => {
+  app.post('/user',UserController.create)
+  app.put('/user/:userId',UserController.put)
+  app.delete('/user/:userId',UserController.remove)
+  app.get('/user/:userId',UserController.show)
+  app.get('/users',UserController.index)
 }
